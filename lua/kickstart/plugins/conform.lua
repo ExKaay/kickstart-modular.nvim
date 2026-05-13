5e501bd4779bf9e4ae1633a859b76a35342b47c1
ExKay <36263590+ExKaay@users.noreply.github.com>
2026-05-13 13:44:11 +0200

    conform: replaced beautysh with shfmt
    Still need to choose the right formatter for me

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -38,7 +38,8 @@ return {
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        bash = { 'beautysh', 'shellharden' },
+        -- bash = { 'beautysh', 'shellharden' },
+        bash = { 'shfmt', 'shellharden' },
       },
     },
   },
