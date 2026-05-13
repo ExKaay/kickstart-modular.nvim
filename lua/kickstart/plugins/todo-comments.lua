5b42896af2710fb2b565466edbb5aab4e0295be5
ExKay <36263590+ExKaay@users.noreply.github.com>
2026-05-13 11:48:31 +0200

    recolored INFO
    Disabled merging keywords to added all default keywords as custom ones to be able to recolor INFO to green

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -11,18 +11,29 @@ return {
     ---@diagnostic disable-next-line: missing-fields
     opts = {
       signs = true,
-      merge_keywords = false,
+      merge_keywords = false, -- needed to recolor defaults
       keywords = {
+
+        -- HACK: These are the default keywords (minus INFO), so that i can _not_ merge keyword, as otherwise i can't recolor INFO
+        FIX = {
+          icon = ' ', -- icon used for the sign, and in search results
+          color = 'error', -- can be a hex color, or a named color (see below)
+          alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
+          -- signs = false, -- configure signs for some keywords individually
+        },
+        TODO = { icon = ' ', color = 'info' },
+        HACK = { icon = ' ', color = 'warning' },
+        WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+        PERF = { icon = ' ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+        NOTE = { icon = ' ', color = 'hint' },
+        TEST = { icon = '⏲ ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+        -- END OF DEFAULTS
+
         -- OLD, used for Comments that are no longer true / fitting
-        OLD = {
-          icon = '󱦟',
-          color = '#C99404',
-          alt = { 'RELICT', 'DEFUNCT', 'IGNORE', 'FALSE' },
-        },
+        OLD = { icon = '󱦟', color = '#C99404', alt = { 'RELICT', 'DEFUNCT', 'IGNORE', 'FALSE' } },
+
         -- Override INFO with a new color, otherwise it becomes unnessecary when i otherwise use NOTE
-        INFO = {
-          color = '#229944',
-        },
+        INFO = { color = '#229944' },
       },
     },
   },
