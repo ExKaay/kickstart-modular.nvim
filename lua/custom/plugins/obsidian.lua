19580319515f1695be27f8cc1f003079cba11b3f
ExKay <36263590+ExKaay@users.noreply.github.com>
2026-06-03 01:28:56 +0200

    Configured obsidian.nvim
    But we are still not finished

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -5,10 +5,52 @@ return {
   ---@type obsidian.config
   opts = {
     legacy_commands = false, -- this will be removed in 4.0.0
+    attachments = {
+      folder = './',
+      -- FIXME: obsidian.builtin is not found but in autocomplete
+      -- img_text_func = require('obsidian.builtin').img_text_func,
+      img_name_func = function() return string.format('Pasted Image %s', os.date '%Y%m%d%H%M%S') end,
+      confirm_img_paste = true,
+      -- NOTE: Always use basename for imagenames
+      --   img_text_func = function(path)
+      --     local name = vim.fs.basename(tostring(path))
+      --     local encodedname = require('obsidian.util').urlencode(name)
+      --     return string.format('![%s](%s)', name, encodedname)
+      --   end,
+    },
+    link = {
+      format = 'relative',
+    },
+    templates = {
+      folder = '03 - Templates',
+      date_format = 'YYYY-MM-DD',
+      time_format = 'hh:mm',
+      substitutions = {},
+    },
     workspaces = {
       {
         name = 'GSynced Vault',
         path = '~/Documents/Obsidian/GSynced Vault',
+      },
+      {
+        name = 'no-vault',
+        path = function()
+          -- NOTE: Alternatively, use CWD:
+          -- return assert(vim.fn.getcwd())
+          return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
+        end,
+        -- FIXME: WARNINGS!
+        --@diagnostics disable-lines
+        overrides = {
+          notes_subdir = vim.NIL,
+          new_notes_location = 'current_dir',
+          templates = {
+            folder = vim.NIL,
+          },
+          frontmatter = {
+            disabled = true,
+          },
+        },
       },
       --   {
       --     name = "work",
--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -19,6 +19,12 @@ return {
     -- scroll = { enabled = true },
     -- statuscolumn = { enabled = true },
     -- words = { enabled = true },
-    images = { enabled = true },
+    images = {
+      enabled = true,
+      resolve = function(path, src)
+        local api = require 'obsidian.api'
+        if api.path_is_note(path) then return api.resolve_attachment_path(src) end
+      end,
+    },
   },
 }
