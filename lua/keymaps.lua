1a2dba12189ff4e2cfd14d8a1dc91ad3da1f795b
ExKay <36263590+ExKaay@users.noreply.github.com>
2026-06-18 19:11:03 +0200

    Changed up/down to scroll
    Changed up and down arrow from "blocked" to C-u and C-d to scroll more
    easily with one hand

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -37,8 +37,11 @@ vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' }
 -- TIP: Disable arrow keys in normal mode
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!"<CR>')
+-- NOTE: I'm using up and down as halfscreen scroll
+-- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!"<CR>')
+-- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!"<CR>')
+vim.keymap.set('n', '<up>', '<C-u>')
+vim.keymap.set('n', '<down>', '<C-d>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
