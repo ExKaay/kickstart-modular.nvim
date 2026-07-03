9c8f28e2507c9121462931b0dc9c3e1b38e85621
ExKay <36263590+ExKaay@users.noreply.github.com>
2026-07-03 10:52:40 +0200

    Fixed obsidian.nvim
    Fixed the usage outside of vaults

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -40,7 +40,7 @@ return {
           -- return assert(vim.fn.getcwd())
           return assert(vim.fs.dirname(vim.api.nvim_buf_get_name(0)))
         end,
-        overrides = {
+        override = {
           notes_subdir = vim.NIL, -- have to use 'vim.NIL' instead of 'nil'
           new_notes_location = 'current_dir',
           templates = {
