8bb61b75af7a86bd77900f58b2e80a7e9ffaaced
ExKay <36263590+ExKaay@users.noreply.github.com>
2026-07-08 12:29:58 +0200

    Changed orgfiles dir
    ~/orgfile -> ~/Documents/orgfiles

--- lua/custom/plugins/orgmode.lua
+++ lua/custom/plugins/orgmode.lua
@@ -6,8 +6,8 @@ return {
   config = function()
     -- Setup orgmode
     require('orgmode').setup {
-      org_agenda_files = '~/orgfiles/**/*',
-      org_default_notes_file = '~/orgfiles/refile.org',
+      org_agenda_files = '~/Documents/orgfiles/**/*',
+      org_default_notes_file = '~/Documents/orgfiles/refile.org',
       mappings = {
         prefix = '<leader>O',
       },
