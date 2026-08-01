75cd62dc9408fc5a9e55fb9ad8589097cb44efef
ExKay <36263590+ExKaay@users.noreply.github.com>
2026-08-02 00:42:44 +0200

    Enabled snacks

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -7,19 +7,43 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    -- bigfile = { enabled = true },
-    -- dashboard = { enabled = true },
+
+    -- CHNG: Lets try bigfile, maybe it wont load _huge_ treesitter instances
+    bigfile = { enabled = true },
+
+    -- CHNG: At least its not so heavywieight
+    dashboard = { enabled = true },
     -- explorer = { enabled = true },
-    -- indent = { enabled = true },
-    -- input = { enabled = true },
+
+    -- CHNG: Animate indentation marks
+    indent = { enabled = true },
+
+    -- WEIRD: Not found until today...
+    input = { enabled = true },
     -- picker = { enabled = true },
     -- notifier = { enabled = true },
-    -- quickfile = { enabled = true },
-    -- scope = { enabled = true },
-    -- scroll = { enabled = true },
+
+    -- CHNG: Render File before loading modules
+    quickfile = { enabled = true },
+
+    -- CHNG: Algo based selection?
+    scope = { enabled = true },
+
+    -- CHNG: Maybe C-d scrolling is now not so disorienting?
+    scroll = {
+      enabled = true,
+      -- REMARK: a bit nicer then standard linear
+      easing = 'sine',
+    },
+
+    -- WEIRD: Already enabled???
     -- statuscolumn = { enabled = true },
-    -- words = { enabled = true },
-    images = {
+
+    -- CHNG: LSP integration?
+    words = { enabled = true },
+
+    -- PERF: *FINALLY* => it was called 'image', not 'images'...
+    image = {
       enabled = true,
       resolve = function(path, src)
         local api = require 'obsidian.api'
